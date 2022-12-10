3561f29caf067717c6f0b7c4b1060d821abe9c5a
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-10 18:37:18 +0900

    feat : 추가 사용자 입력에 관한 예외 처리
    사용자 입력에 관한 예외처리 코드 구현.

---
+++ src/main/java/baseball/model/InputException.java
@@ -0,0 +1,20 @@
+package baseball.model;
+
+public class InputException {
+    public void isBaseballNumber(String input) throws IllegalArgumentException{
+        if(input.replaceAll("[^0-9]", "").length() != input.length()){
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+        if(input.length()!=3){
+            throw new IllegalArgumentException("숫자를 3개만 입력해주세요.");
+        }
+    }
+    public void isRestartOrEndNumber(String input) throws IllegalArgumentException{
+        if(input.replaceAll("[^0-9]", "").length() != input.length()){
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+        if(input.equals("1")||input.equals("2")){
+            throw new IllegalArgumentException("1과 2로 입력해주세요");
+        }
+    }
+}
