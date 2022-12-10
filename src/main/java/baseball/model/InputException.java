ad59fbfbdcffd8ba6cb553e38cf7b1d8d9075fdb
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-10 19:11:40 +0900

    feat : 수정 숫자범위
    사용자의 입력이 1에서 9까지 허용되는 것을 반영.

--- src/main/java/baseball/model/InputException.java
+++ src/main/java/baseball/model/InputException.java
@@ -2,8 +2,8 @@ package baseball.model;
 
 public class InputException {
     public void isBaseballNumber(String input) throws IllegalArgumentException{
-        if(input.replaceAll("[^0-9]", "").length() != input.length()){
-            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        if(input.replaceAll("[^1-9]", "").length() != input.length()){
+            throw new IllegalArgumentException("1과 9사이 숫자를 입력해주세요.");
         }
         if(input.length()!=3){
             throw new IllegalArgumentException("숫자를 3개만 입력해주세요.");
