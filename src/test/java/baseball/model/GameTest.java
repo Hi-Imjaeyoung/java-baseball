3ef98d2b841446d8ea069207485947e87d36dfb0
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-10 19:43:42 +0900

    test : 추가 사용자의 값과 컴퓨터의 값을 비교하는 기능 테스트

---
+++ src/test/java/baseball/model/GameTest.java
@@ -0,0 +1,44 @@
+package baseball.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameTest {
+    @DisplayName("숫자야구 사용자 입력")
+    @Test
+    void CONVERT(){
+        Game game = new Game(List.of(1,2,3));
+        List<Integer> expect = List.of(1,2,3);
+        assertThat(game.convertNumbers("123")).containsExactly(1,2,3);
+    }
+    @DisplayName("숫자야구 게임 결과")
+    @Test
+    void NOTHING(){
+        Game game = new Game(List.of(1,2,3));
+        assertThat("낫싱").isEqualTo(game.compareNumbers("456"));
+    }
+    @Test
+    void BALL(){
+        Game game = new Game(List.of(1,2,3));
+        assertThat("1볼").isEqualTo(game.compareNumbers("245"));
+        assertThat("2볼").isEqualTo(game.compareNumbers("235"));
+        assertThat("3볼").isEqualTo(game.compareNumbers("231"));
+    }
+    @Test
+    void STRIKE(){
+        Game game = new Game(List.of(1,2,3));
+        assertThat("1스트라이크").isEqualTo(game.compareNumbers("145"));
+        assertThat("2스트라이크").isEqualTo(game.compareNumbers("125"));
+        assertThat("3스트라이크").isEqualTo(game.compareNumbers("123"));
+    }
+    @Test
+    void BALLANDSTRIKE(){
+        Game game = new Game(List.of(1,2,3));
+        assertThat(game.compareNumbers("142")).isEqualTo("1볼 1스트라이크");
+    }
+}
