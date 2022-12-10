56d2202d63f8d51e45a6d78a13589aad198595f1
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-10 18:31:12 +0900

    feat : 추가 사용자 입력에 관한 기능
    사용자 입력에 관한 코드 구현.

---
+++ src/main/java/baseball/veiw/InputVeiw.java
@@ -0,0 +1,14 @@
+package baseball.veiw;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputVeiw {
+    public String inputUserBaseballNumber(){
+        String input = Console.readLine();
+        return input;
+    }
+    public String inputUserContinue(){
+        String input = Console.readLine();
+        return input;
+    }
+}
