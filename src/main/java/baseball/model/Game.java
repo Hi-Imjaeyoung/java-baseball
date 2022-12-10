fc1c1ecf0e418d320eac5afa837aa69a4e254f01
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-10 19:43:18 +0900

    feat : 추가 사용자의 값과 컴퓨터의 값을 비교하는 기능

--- src/main/java/baseball/model/Game.java
+++ src/main/java/baseball/model/Game.java
@@ -1,15 +1,55 @@
 package baseball.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
     private List<Integer> computerNumber;
-
+    private int strike,ball;
     public Game(List<Integer> numbers){
-            this.computerNumber = numbers;
+        this.computerNumber = numbers;
+        this.strike =0;
+        this.ball =0;
     }
-    public void compareNumbers(String input){
-
+    public String compareNumbers(String input){
+        resetCount();
+        List<Integer> inputNumbers = convertNumbers(input);
+        for(int i=0;i<3;i++){
+            int nowNumber = inputNumbers.get(i);
+            if(computerNumber.contains(nowNumber))
+                isStrikeOrBall(nowNumber,i);
+        }
+        return result();
+    }
+    public List<Integer> convertNumbers(String input){
+        List<Integer> inputNumbers = new ArrayList<>();
+        for(int i=0 ;i<3;i++){
+            inputNumbers.add(Character.getNumericValue(input.charAt(i)));
+        }
+        return inputNumbers;
+    }
+    private void isStrikeOrBall(int number,int index){
+        if(computerNumber.get(index)==number){
+            strike = strike+1;
+            return;
+        }
+        ball=ball+1;
+    }
+    private void resetCount(){
+        strike = 0;
+        ball = 0;
+    }
+    private String result(){
+        if(strike==0 && ball ==0){
+            return "낫싱";
+        }
+        if(ball==0){
+            return strike+"스트라이크";
+        }
+        if(strike==0){
+            return ball+"볼";
+        }
+        return ball+"볼 "+strike+"스트라이크" ;
     }
 
 }
